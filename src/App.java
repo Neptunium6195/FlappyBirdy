e18c985600fdcdb0ecd2e299680a14d521835285
Neptunium6195 <seratesarah@gmail.com>
2026-05-14 09:36:47 -0700

    Birdy
    gravity, velocity, movement

--- src/App.java
+++ src/App.java
@@ -13,8 +13,8 @@ public class App {
         FlappyBird flappyBird = new FlappyBird();
         frame.add(flappyBird);
         frame.pack();
+        flappyBird.requestFocus();
         frame.setVisible(true);
-
 
     }
 }
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,6 +1,7 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class FlappyBird extends JPanel{
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -8,6 +9,30 @@ public class FlappyBird extends JPanel{
     Image birdyImg;
     Image topPipeImg;
     Image bottomPipeImg;
+
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    int yVelocity = 0;
+    int gravity = 1;
+
+    Bird bird;
+    Timer gameLoopTimer;
+
+    public class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+        
+    }
 
     FlappyBird(){
         bgImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
@@ -17,16 +42,49 @@ public class FlappyBird extends JPanel{
 
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.green);
+        setFocusable(true);
+        addKeyListener(this);
 
+        bird = new Bird(birdyImg);
+        gameLoopTimer = new Timer(1000/60, this);
+        gameLoopTimer.start();
+
+    }
+
+    public void move(){
+        yVelocity += gravity;
+        bird.y += yVelocity;
+        bird.y = Math.max(bird.y, 0);
     }
 
     public void draw(Graphics g) {
         g.drawImage(bgImg, 0, 0, boardWidth, boardHeight, null);
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+        //System.out.println("hi");
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         draw(g);
     }
+
+    @Override //ew #VEX reference lololol
+    public void actionPerformed(ActionEvent e){
+        move();
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            yVelocity =  -9;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
 }
